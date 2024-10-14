9ed091cb2f871929a503f213638aa1157b04b987
KelvinVo24 <106607398+KelvinVo24@users.noreply.github.com>
2024-10-14 16:07:57 +0700

    Change the prompt and add some statistics

--- src/app/statistics/[gameId]/page.tsx
+++ src/app/statistics/[gameId]/page.tsx
@@ -1,4 +1,5 @@
 import AccuaracyCard from "@/components/statistics/AccuaracyCard";
+import QuestionList from "@/components/statistics/QuestionList";
 import ResultsCard from "@/components/statistics/ResultsCard";
 import TimeTakenCard from "@/components/statistics/TimeTakenCard";
 import { buttonVariants } from "@/components/ui/button";
@@ -24,10 +25,32 @@ const StatisticsPage = async ({ params: { gameId } }: Props) => {
     where: {
       id: gameId,
     },
+    include: {
+      questions: true,
+    },
   });
   if (!game) {
     return redirect("/quiz");
   }
+
+  let accuaracy = 0;
+  if (game.gameType == "mcq") {
+    let totalCorrect = game.questions.reduce((acc, question) => {
+      if (question.isCorrect) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+    accuaracy = (totalCorrect / game.questions.length) * 100;
+  } else if (game.gameType === "open_ended") {
+    let totalPercentage = game.questions.reduce((acc, question) => {
+      return acc + (question.percenttageCorrect || 0);
+    }, 0);
+
+    accuaracy = (totalPercentage / game.questions.length) * 100;
+  }
+
+  accuaracy = Math.round(accuaracy * 100) / 100;
   return (
     <>
       <div className="p-8 mx-auto max-w-7xl">
@@ -41,11 +64,14 @@ const StatisticsPage = async ({ params: { gameId } }: Props) => {
           </div>
         </div>
         <div className="grid gap-4 mt-4 md:grid-cols-7">
-          <ResultsCard accuaracy={80} />
-          <AccuaracyCard accuaracy={80} />
-          <TimeTakenCard timeEnded={new Date()} timeStarted={new Date()} />
+          <ResultsCard accuaracy={accuaracy} />
+          <AccuaracyCard accuaracy={accuaracy} />
+          <TimeTakenCard
+            timeEnded={new Date()}
+            timeStarted={game.timeStarted}
+          />
         </div>
-        {/* <QuestionList /> */}
+        <QuestionList questions={game.questions} />
       </div>
     </>
   );
