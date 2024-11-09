e54666f3e296c751e67be453cb979a2aa96e8250
KelvinVo24 <106607398+KelvinVo24@users.noreply.github.com>
2024-11-09 19:55:22 +0700

    Add more elements in Quiz and History

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { CardContent, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const BlogList: React.FC = () => {
   return (
@@ -20,13 +22,16 @@ const BlogList: React.FC = () => {
                 Simple Present Tense: Examples and How to use it
               </h4>
             </a>
-            <p className="mb-2 text-sm">
+            <p className="mb-2 text-sm text-justify">
               Verb tenses in English are quite challenging to learn and master.
               They sometimes confuse language learners. The present tense in
               English is not an exception. It has its own structure and
               appropriate use that are usually misused by students.
             </p>
-            <img src="/simple_present.jpg" className="w-100" />
+            <img
+              src="/simple_present.jpg"
+              className="w-full h-52 object-cover object-center"
+            />
             <hr className="mt-4" />
             <span className="text-xs">ARTICLE</span>
             &nbsp;<span className="text-xs text-gray-500">PROCESS</span>
@@ -44,13 +49,16 @@ const BlogList: React.FC = () => {
                 10 Ways To Improve Your Listening Skills (With Examples)
               </h4>
             </a>
-            <p className="mb-2 text-sm">
+            <p className="mb-2 text-sm text-justify">
               Strong communication skills are a valued trait in every industry
               and listening skills are an essential part of effective
               communication. It can be beneficial to assess and strengthen your
               listening skills in professional situations and beyond.
             </p>
-            <img src="/listening.png" className="w-100" />
+            <img
+              src="/listening.png"
+              className="w-full h-52 object-cover object-center"
+            />
             <hr className="mt-4" />
             <span className="text-xs">ARTICLE</span>
             &nbsp;<span className="text-xs text-gray-500">PROCESS</span>
@@ -68,19 +76,41 @@ const BlogList: React.FC = () => {
                 How to learn English quickly: 10 tips
               </h4>
             </a>
-            <p className="mb-2 text-sm">
+            <p className="mb-2 text-sm text-justify">
               English is a fun language to learn (here are 10 reasons why it
               rocks), and even though it’s considered an accessible and
               relatively easy one to learn, with 750,000 words and spelling that
               can throw off even the most skilled learner, learning English fast
               can seem impossible.
             </p>
-            <img src="/book.png" className="w-100" />
+            <img
+              src="/book.png"
+              className="w-full h-52 object-cover object-center"
+            />
             <hr className="mt-4" />
             <span className="text-xs">ARTICLE</span>
             &nbsp;<span className="text-xs text-gray-500">PROCESS</span>
           </div>
         </div>
+      </div>
+      <div className="flex justify-end mt-8">
+        <Link
+          href="/Blog"
+          className="flex items-center text-indigo-700 border border-indigo-600 py-2 px-6 gap-2 rounded inline-flex items-center hover:bg-indigo-700 hover:text-white transition duration-300"
+        >
+          <span>View More</span>
+          <svg
+            className="w-6 h-6 ml-2"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+          </svg>
+        </Link>
       </div>
     </div>
   );
